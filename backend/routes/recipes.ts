81cd0f037e01beb08104487aeffa26c690da8110
Mia Dinh <miadinh38@gmail.com>
2024-05-02 22:48:46 -0400

    Create routes folder and implement routes for recipes

---
+++ backend/routes/recipes.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+const router = require('express').Router();
+
+
+// Get all recipes
+router.get('/', (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error((error as Error).message);
+  }
+})
+
+// Create new recipe
+router.post('/', (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error((error as Error).message)
+  }
+})
+
+// Update recipe
+router.put('/:id', (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error((error as Error).message)
+  }
+})
+
+// Delete recipe
+router.delete('/:id', (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error((error as Error).message)
+  }
+})››
