e0cdfc8d35ce948900dc35eee6806850feb67805
Mia Dinh <miadinh38@gmail.com>
2024-05-03 11:06:56 -0400

    Add connection file

---
+++ backend/db/connection.ts
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+
+
+const dbParams =  {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME
+};
+
+const db = new Pool(dbParams);
+
+module.exports = db;
