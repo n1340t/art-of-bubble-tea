22e34d7003f6d14238678bfc1c82fb078e965a2b
Mia Dinh <miadinh38@gmail.com>
2024-05-10 20:26:37 -0400

    Fix param name in getRandomOrder func

--- backend/routes/orders.ts
+++ backend/routes/orders.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { CMF } from "../db/seeds/orders";
+import { KF_TEA_LIST } from "../db/seeds/orders";
 import getRandomOrder from "../queries/orders";
 
 const ordersRouter = Router();
@@ -7,7 +7,7 @@ const ordersRouter = Router();
 // Get random oder
 ordersRouter.get("/", async(req: Request, res: Response) => {
   try {
-    const orderResult = await getRandomOrder(CMF);
+    const orderResult = await getRandomOrder(KF_TEA_LIST);
     res.status(200).json(orderResult);
   } catch (error) {
     console.error((error as Error).message);
