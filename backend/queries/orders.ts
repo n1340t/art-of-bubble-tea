ec6fda9db71907a31bba06c5f906769e6ff6741a
Mia Dinh <miadinh38@gmail.com>
2024-05-10 20:27:52 -0400

    Add egde case for getRandomItem func

--- backend/queries/orders.ts
+++ backend/queries/orders.ts
@@ -1,19 +1,24 @@
-import { DrinkData } from "../db/seeds/orders";
+import { DrinkData } from "../db/schema/orderTypes";
+
+const getRandomItem = (array: string[] | null) => {
+  if (!array || array.length === 0) {
+    return null;
+  }
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
 
 const getRandomOrder = (drinks: DrinkData[]) => {
   const randomIndex = Math.floor(Math.random() * drinks.length);
   const randomDrink = drinks[randomIndex];
-  const getRandomItem = (array: string[]) =>
-    array[Math.floor(Math.random() * array.length)];
 
   const result = {
     name: randomDrink.name,
-    bigToppings: getRandomItem(randomDrink.bigToppings),
-    smallToppings: getRandomItem(randomDrink.smallToppings),
+    size: getRandomItem(randomDrink.size),
+    sweetness: getRandomItem(randomDrink.sweetness),
+    ice: getRandomItem(randomDrink.ice),
+    topping: getRandomItem(randomDrink.topping),
     milk: getRandomItem(randomDrink.milk),
-    sweetnessLevel: getRandomItem(randomDrink.sweetnessLevel),
-    iceLevel: getRandomItem(randomDrink.iceLevel),
-    cupSize: getRandomItem(randomDrink.cupSize),
   };
 
   return result;
