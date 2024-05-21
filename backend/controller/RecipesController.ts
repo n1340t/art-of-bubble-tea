205d7f9e1e81b7b6fcbca617b23ed1124791f187
Le An Nguyen <nguyenlean96@gmail.com>
2024-05-21 02:50:40 -0400

    Recipe Controller init

---
+++ backend/controller/RecipesController.ts
@@ -0,0 +1,27 @@
+const index = () => {
+  return;
+}
+
+const update = () => {
+  return;
+}
+
+const store = () => {
+  return;
+}
+
+const show = () => {
+  return;
+}
+
+const destroy = () => {
+  return;
+}
+
+export {
+  index,
+  update,
+  store,
+  show,
+  destroy
+}
